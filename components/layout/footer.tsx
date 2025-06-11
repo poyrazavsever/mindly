d94ec62b98c455a6b1d2ce18dd09e0bd8a865908
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-11 11:29:03 +0300

    feat: created simple footer for now

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="w-full py-6 text-primary text-center">
+      <span className="text-sm font-medium">
+        © {new Date().getFullYear()} Mindly. All rights reserved.
+      </span>
+    </footer>
   )
 }
 
